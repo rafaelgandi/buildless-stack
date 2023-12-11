6a7f8ed9363dac4333104e641315373a0a26eb0b
rafaelgandi.mac <rafaelgandi@gmail.com>
2023-12-11 16:10:02 +0800

    update server.js script

--- server.js
+++ server.js
@@ -1,49 +1,46 @@
 // Run this to develop locally. //
 
+// From AI
+const http = require('http');
+const fs = require('fs').promises;
+const path = require('path');
+const mime = require('mime');
 
-if (typeof Bun !== 'undefined' && Bun?.file) {
-    const BASE_PATH = "./public";
-    const server = Bun.serve({
-        port: 8000,
-        fetch(req) {
-            const pathname = new URL(req.url).pathname ?? '';
-            const filePath = BASE_PATH + ((pathname.trim().replace('/', '') !== '') ? pathname : '/index.html');
-            const file = Bun.file(filePath);
-            return new Response(file);
-        },
-    });
-    console.log(`🌈 Listening on localhost: ${server.port}`);
-}
-else {
-    const http = require('http');
-    const fs = require('fs');
-    const path = require('path');
-    const mime = require('mime');
+const server = http.createServer(async (req, res) => {
+    // Get the file path of the requested resource
+    const filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
 
-    const BASE_PATH = "./public";
-    const PORT = 8000;
+    try {
+        // Read the file
+        const data = await fs.readFile(filePath);
 
-    const server = http.createServer((req, res) => {
-        const { pathname } = new URL(req.url, `http://${req.headers.host}`);
-        const filePath = path.join(BASE_PATH, pathname === '/' ? 'index.html' : pathname);
+        // Determine the content type based on the file extension
+        const contentType = mime.getType(filePath) || 'application/octet-stream';
+        res.setHeader('Content-Type', contentType);
 
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                res.writeHead(404, { 'Content-Type': 'text/plain' });
-                res.end('Not found');
-            } else {
-                const contentType = mime.getType(filePath) || 'text/plain';
-                res.writeHead(200, { 'Content-Type': contentType });
+        res.statusCode = 200;
+        res.end(data);
+    } catch (err) {
+        // Send the index.html file for any non-existing routes
+        if (err.code === 'ENOENT') {
+            try {
+                const data = await fs.readFile(path.join(__dirname, 'public', 'index.html'));
+
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
                 res.end(data);
+            } catch (err) {
+                res.statusCode = 500;
+                res.end('Internal server error');
             }
-        });
-    });
+        } else {
+            res.statusCode = 500;
+            res.end('Internal server error');
+        }
+    }
+});
 
-    server.listen(PORT, () => {
-        console.log(`🌈 Listening on localhost:${PORT}`);
-    });
-}
-
-
-
-
+const port = 8000;
+server.listen(port, () => {
+    console.log(`🌈 Server running on port ${port}`);
+});
