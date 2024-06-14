b49e8a97a328875e51474dfc4f8d773738d25feb
Rafael Gandionco <raffy@photoup.net>
2024-06-14 11:47:52 +0800

    updates

--- public/src/components/Foobar/Foobar.js
+++ public/src/components/Foobar/Foobar.js
@@ -4,9 +4,19 @@ import './Foobar.styles.js';
 /** @param {import("./Foobar.d.ts").FoobarProps} props */
 export default function Foobar(props) {
     return html`
-        <h1 class="_26c1c2a7">
-            ${props.greet}
-            <sub>buildless-stack at your service</sub>
-        </h1>
+       <>
+            <h1 class="_26c1c2a7">
+                ${props.greet}
+                <sub>buildless-stack at your service</sub>
+            </h1>
+            <div style=${{textAlign: 'center'}}>
+                <sl-badge variant="primary" pulse pill>Primary</sl-badge>
+                <sl-badge variant="success" pulse pill>Success</sl-badge>
+                <sl-badge variant="neutral" pulse pill>Neutral</sl-badge>
+                <sl-badge variant="warning" pulse pill>Warning</sl-badge>
+                <sl-badge variant="danger" pulse pill>Danger</sl-badge>
+            </div>
+            
+        </>
     `;
 }
