1ea2efe0a9570c292bcac67fe47f505943ce1933
rafaelgandi.mac <rafaelgandi@gmail.com>
2025-06-03 15:54:27 +0800

    added guard functions for asynchronous and synchronous error handling in helpers.js

--- public/src/lib/helpers.js
+++ public/src/lib/helpers.js
@@ -1,5 +1,3 @@
-
-
 export function isValidJson(jsonString) {
     try {
         JSON.parse(jsonString);
@@ -10,6 +8,37 @@ export function isValidJson(jsonString) {
     }
 }
 
+export async function guard(func) {
+    try {
+        const res = await func();
+        return {
+            result: res,
+            error: null
+        };
+    }
+    catch (err) {
+        return {
+            result: null,
+            error: err
+        };
+    }
+}
 
+// For syncronous guarding
+guard.let = function (func) {
+    try {
+        const res = func();
+        return {
+            result: res,
+            error: null
+        };
+    }
+    catch (err) {
+        return {
+            result: null,
+            error: err
+        };
+    }
+};
 
 
